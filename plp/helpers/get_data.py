d11d5730d6daa8a4846a5affbf23131073a3e73c
Moritz Huesser <morihues0@gmail.com>
2021-10-26 15:06:40 +0200

    plp nn setup

--- plp/helpers/get_data.py
+++ plp/helpers/get_data.py
@@ -1,52 +1,64 @@
 from ast import literal_eval
 from math import log10, floor
-
-def getData():
-    X = getX()
-    y = getY()
-
-    X = roundX(X)
-    y = roundY(y)
-
-    return [X, y]
-
-def round_sig(x, sig=5):
-    if(x == 0): return 0
-    return round(x, sig-int(floor(log10(abs(x))))-1)
-
-def roundX(X):
-    newX = []
-    for vals in X:
-        newVals = []
-        for val in vals:
-            newVals.append(round_sig(val))
-        newX.append(newVals)
-    return newX
-
-def roundY(y):
-    newY = []
-    for val in y:
-        newY.append(round_sig(val))
-    return newY
-
-def getY():
-    y = []
-    yf = open("plp/data/training_raw/Y.txt", "r")
-
-    for line in yf:
-        y.append(literal_eval(line))
-    
-    return y
-
-def getX():
-    X = []
-
-    Xf = open("plp/data/training_raw/X.txt", "r")
-    for line in Xf:
-        X.append(literal_eval(line))
-
-    return X
+import numpy as np
 
 
 
-getData()
+class data:
+    def get(self):
+        X = self.getX()
+        y = self.getY()
+
+        X = self.roundX(X)
+        y = self.roundY(y)
+
+        length = len(X)
+        train_amount = length - 10 
+
+        X_train = X[:train_amount]
+        X_test = X[train_amount:]
+        y_train = X[:train_amount]
+        y_test = X[train_amount:]
+
+
+        return [[X_train, y_train], [X_test, y_test]]
+
+    def round_sig(self,x, sig=5):
+        if(x == 0): return 0
+        return round(x, sig-int(floor(log10(abs(x))))-1)
+
+    def roundX(self,X):
+        newX = []
+        for vals in X:
+            newVals = []
+            for val in vals:
+                newVals.append(self.round_sig(val))
+            newX.append(newVals)
+        return newX
+
+    def roundY(self,y):
+        newY = []
+        for val in y:
+            newY.append(self.round_sig(val))
+        return newY
+
+    def getY(self):
+        y = []
+        yf = open("plp/data/training_raw/Y.txt", "r")
+
+        for line in yf:
+            y.append(literal_eval(line))
+        
+        return y
+
+    def getX(self):
+        X = []
+
+        Xf = open("plp/data/training_raw/X.txt", "r")
+        for line in Xf:
+            X.append(literal_eval(line))
+
+        return X
+
+
+data = data()
---
+++ plp/nn.py
@@ -0,0 +1,7 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+print(tf.__version__)
+
+from helpers.get_data import data
+
+print(len(data.get()))
